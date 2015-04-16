724d216c235175b5be57687226c2fefac469a762
Harry Dowe <harry.dowe@gmail.com>
2015-04-16 19:07:38 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Media;
+using System;
+
+namespace silent_audio {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                SoundPlayer simpleSound = new SoundPlayer("audio.wav");
+                simpleSound.PlaySync();
+            } catch (Exception e) {
+                // Do nothing
+            }
+        }
+    }
+}
